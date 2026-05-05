38898a31b2c23bcf37c0388623bf414db4290f67
joshidoshi <joshua.dietiker@gmail.com>
2026-05-05 18:07:07 +0200

    feat: add light rays to background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
+	import { LightRays } from '$lib/components/magic/light-rays';
 
 	let { children } = $props();
 </script>
@@ -9,5 +10,6 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <ModeWatcher />
 <main class="flex h-screen flex-col items-center justify-center gap-6 text-center">
+	<LightRays />
 	{@render children()}
 </main>
