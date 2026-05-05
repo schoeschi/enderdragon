c6aab8aeb96d8b368c6bf790f43a78c6c396a84a
joshidoshi <joshua.dietiker@gmail.com>
2026-05-05 17:27:35 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<ModeWatcher />
+<main class="flex h-screen flex-col items-center justify-center gap-6">
+	{@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Badge } from '$lib/components/ui/badge';
+	import { Users } from '@lucide/svelte';
+	import * as Card from '$lib/components/ui/card/index.js';
+</script>
+
+<Badge
+	variant="outline"
+	class="border-primary/50 bg-primary/10 text-primary dark:border-white/50 dark:bg-white/30 dark:text-white"
+>
+	<Users /> 2 players online
+</Badge>
+
+<h1 class="max-w-4xl text-5xl font-black tracking-tighter sm:text-7xl lg:text-8xl">
+	Join <span class="text-primary">Enderdragon</span>.
+</h1>
+
+<Card.Root class="p-2 shadow-xl">
+	<Card.Content class="flex items-center gap-2 p-0 pl-2">
+		<code class="font-mono font-bold text-primary dark:text-white">mc.enderdragon.ch</code>
+		<Button size="sm">Copy IP</Button>
+	</Card.Content>
+</Card.Root>
